b382752043cd2bd986f3751f55ef14d352e99f86
Ubuntu <ubuntu@ip-172-31-18-176.eu-west-1.compute.internal>
2017-10-22 15:12:44 +0000

    inventory

--- gamefast/gamefast/settings.py
+++ gamefast/gamefast/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'challenge',
     'rest_framework',
     'rest_framework.authtoken',
+    'inventory',
 ]
 
 MIDDLEWARE = [
---
+++ gamefast/inventory/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.conf import settings
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+class Inventory(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return u'{0}\'s inventory'.format(self.user)
+
+class Item (models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(null=True, blank=True, height_field=256, width_field=256)
+    stored_in = models.ManyToManyField(
+        Inventory,
+        null = True,
+        blank = True,
+        related_name = 'items'
+    )
+
+    def __str__(self):
+        return self.name;
+
+    def add(self, user):
+        user.inventory.items.add(self)
+        user.inventory.save()
+    def remove(self):
+        user.inventory.items.delete(self)
+        user.inventory.save()
+    def edit(self): pass
+    def show(self):
+        return self;
